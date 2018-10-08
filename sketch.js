ae848241939a2e5f8ae5cb1e1cd3f8bc42b7420c
Ndollar1 <43085317+Ndollar1@users.noreply.github.com>
2018-10-08 16:28:46 -0700

    Changed to be vertical layout

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var d;
 var tspeed;
 var cspeed;
-var truckx =600;
+var trucky =10;
 var carx = 400;
 var tacc;
 var cacc;
@@ -47,42 +47,44 @@ function draw() {
 
   text(tspeed,50,500);
   //measures distance from car to back of truck;
-  d = int((dist(mouseX+35,mouseY,truckx,125)/4));
+  d = int((dist(mouseX,mouseY-25,425,trucky+200)/4));
+  stroke(255,0,0);
+  line(mouseX,mouseY-25,425,trucky+200);
   //truck boundaries
   stroke(0);
   fill(255,0,0,0);
-  rect(truckx,100,200,50);
+  rect(400,trucky,50,200);
 
   noStroke();
   //danger 1 zone behind truck
   fill(250,250,0,100);
-  rect(truckx-tspeed,100,-tspeed,50);
+  rect(400,trucky+tspeed+200,50,tspeed);
    //danger 2 zone behind truck
   fill(250,0,0,100);
-  rect(truckx,100,-tspeed,50);
+  rect(400,trucky+200,50,tspeed);
 
   //pc car boundaries
-  stroke(0);
-  fill(255,0,0,0);
-  rect(carx,150,100,50);
-
-  noStroke();
-  //danger 1 zone behind car
-  fill(250,250,0,100);
-  rect(carx-cspeed,150,-cspeed,50);
-   //danger 2 zone behind car
-  fill(250,0,0,100);
-  rect(carx,150,-cspeed,50);
+  // stroke(0);
+  // fill(255,0,0,0);
+  // rect(carx,150,100,50);
+  //
+  // noStroke();
+  // //danger 1 zone behind car
+  // fill(250,250,0,100);
+  // rect(carx-cspeed,150,-cspeed,50);
+  //  //danger 2 zone behind car
+  // fill(250,0,0,100);
+  // rect(carx,150,-cspeed,50);
   //car stuck to mouse cursor
   fill(255);
-  rect(mouseX-35,mouseY-25,70,50);
+  rect(mouseX-25,mouseY-35,50,70);
   //check for danger 1 zone behind truck
-  if((mouseX+35)>(truckx-(tspeed*2))&&(mouseX+30)<(truckx-tspeed)&&(mouseY-25)<150&&(mouseY+25)>100){
+  if((mouseY-35)>(trucky+200+tspeed)&&(mouseY-30)<(trucky+(tspeed*2))+200&&(mouseX+25)>400&&(mouseX-25)<450){
     fill(255,255,0);
     ellipse(500,500,20,20);
   }
   //check for danger 2 zone behind truck
-  if((mouseX+35)>(truckx-tspeed)&&(mouseX+30)<truckx&&(mouseY-25)<150&&(mouseY+25)>100){
+  if((mouseY-35)>(trucky+200)&&(mouseY-30)<(trucky+200+tspeed)&&(mouseX+25)>400&&(mouseX-25)<450){
     text(d,600,600);
     fill(255,0,0);
     if(frameCount%d == 0){
